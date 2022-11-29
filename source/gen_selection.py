6604b2226559a9ae068bb769cfd6a2cb0a911568
Poolce <poolce@list.ru>
2022-11-29 13:18:45 +0300

    master

--- source/gen_selection.py
+++ source/gen_selection.py
@@ -12,7 +12,7 @@ A_jun = []
 B_jun = []
 A_adult = []
 B_adult = []
-for i in range(n//4):
+for i in range(n):
     for sign in [-1,1]:
         for sign_2 in [-1,1]:
             A_jun.append(random.random()*(-param.depth))
@@ -22,108 +22,63 @@ for i in range(n//4):
             B_adult.append(random.random()* min(-A_adult[i], A_adult[i]+param.depth)*sign_2)
 
 
-
-Macroparameters = [[] for i in range(8)]
-for i in range(n):
-    Macroparameters[0].append(param.sigma1 * (A_jun[i] + param.depth))
-    Macroparameters[1].append(-param.sigma2*(A_jun[i] + param.depth + B_jun[i]/2))
-    Macroparameters[2].append(-2*np.square(math.pi*B_jun[i]))
-    Macroparameters[3].append(-(np.square(A_jun[i]+param.optimal_depth)-np.square(B_jun[i])/2))
-    Macroparameters[4].append(param.sigma1*(A_adult[i] + param.depth))
-    Macroparameters[5].append(-param.sigma2*(A_adult[i] + param.depth + B_adult[i]/2))
-    Macroparameters[6].append(-2*np.square(math.pi*B_adult[i]))
-    Macroparameters[7].append(-(np.square(A_adult[i]+param.optimal_depth)-np.square(B_adult[i])/2))
-
-
-
+k = 0
 Fitness = []
-index_of_bad_Mps = []
-for i in range(n):
-    p = param.alpha_j*Macroparameters[0][i] + param.beta_j*Macroparameters[2][i] + param.delta_j*Macroparameters[3][i]
-    r = param.alpha_a*Macroparameters[4][i] + param.beta_a*Macroparameters[6][i] + param.delta_a*Macroparameters[7][i]
-    q = param.gamma_j*Macroparameters[1][i]
-    s = param.gamma_a*Macroparameters[5][i]
+for i in range(4*n):
+    res = []
 
-    if((4*r*p+np.square(p+q+s))<0):
-        index_of_bad_Mps.append(i)
-    else:
-        Fitness.append(-s-p-q+(np.sqrt((4*r*p+np.square(p+q+s)))))
+    M1 = param.sigma1 * (A_jun[i] + param.depth)
+    M2 = -param.sigma2*(A_jun[i] + param.depth + B_jun[i]/2)
+    M3 = -2*np.square(math.pi*B_jun[i])
+    M4 = -(np.square(A_jun[i]+param.optimal_depth)-np.square(B_jun[i])/2)
+    M5 = param.sigma1*(A_adult[i] + param.depth)
+    M6 = -param.sigma2*(A_adult[i] + param.depth + B_adult[i]/2)
+    M7 = -2*np.square(math.pi*B_adult[i])
+    M8 = -(np.square(A_adult[i]+param.optimal_depth)-np.square(B_adult[i])/2)
 
-#Очистка
-k=0
-for i in range(len(index_of_bad_Mps)):
-    for j in range(8):
-        Macroparameters[j].pop(i+k)
-    k-=1
+    p = param.alpha_j*M1 + param.beta_j*M3 + param.delta_j*M4
+    r = param.alpha_a*M5 + param.beta_a*M7 + param.delta_a*M8
+    q = param.gamma_j*M2
+    s = param.gamma_a*M6
+    if(4*r*p+np.square(p+q-s)>=0):
 
-        
-
-
-# M_diff = [[[]for i in range(len(Macroparameters[1]))] for j in range(len(Macroparameters[1]))]
-
-# for v in range(len(Macroparameters[1])):
-#     for w in range(len(Macroparameters[1])):
-#         for i in range(8):
-#             M_diff[v][w].append(Macroparameters[i][v]-Macroparameters[i][w])
+        fit = -s-p-q+(np.sqrt((4*r*p+np.square(p+q-s))))
+        res = [fit,M1,M2,M3,M4,M5,M6,M7,M8]
+        k+=1
+        for i in range(8):
+            for j in range(i,8):
+                res.append(res[i+1]*res[j+1])
+        Fitness.append(res)
 
 
-M_sqr = [[[]for i in range(8)] for j in range(8)]
-for i in range(8):
-    for j in range(8):
-        for k in range(len(Macroparameters[1])):
-            M_sqr[j][i].append(Macroparameters[i][k]*Macroparameters[j][k])
-
-
-# M_sqr_diff = [[[[]for i in range(len(Macroparameters[1]))]for j in range(8)]for k in range(8)]
-
-
-# for v in range(len(Macroparameters[1])):
-#     for w in range(len(Macroparameters[1])):
-#         for i in range(8):
-#             for j in range(8):
-#               M_sqr_diff[i][j][v].append(M_sqr[i][j][v]-M_sqr[i][j][w])
-
-# M_diff_norm=[[[]for i in range(len(Macroparameters[1]))] for j in range(len(Macroparameters[1]))]
-# for i in range(8):
-#     M_max=max([(abs(m[i])for m in mp) for mp in M_diff])
-#     for v in range(len(Macroparameters[1])):
-#         for w in range(len(Macroparameters[1])):
-#             M_diff[v][w].append(M_diff[v][w][i]/M_max)
-
-# M_sqr_diff_norm=[[[[]for i in range(len(Macroparameters[1]))]for j in range(len(Macroparameters[1]))]for k in range(8)]
-
-# for i in range(8):
-#     for j in range(8):
-#         M_max=max([(abs(m[i][j])for m in mp) for mp in M_diff])
-#         for v in range(len(Macroparameters[1])):
-#             for w in range(len(Macroparameters[1])):
-#                 M_sqr_diff_norm[v][w][i].append(M_sqr_diff[v][w][i][j]/M_max)
-
-
-# norm_Macroparameters = [[] for i in range(8)]
-# for i in range(8):
-#     max_param = max([abs(mp) for mp in Macroparameters[i]])
-#     for j in range(len(Macroparameters[i])):
-#         norm_Macroparameters[i].append(Macroparameters[i][j]/max_param)
-
-
-
-
-
-
-
+print(k)
 
 
 classification_Table = np.zeros((len(Fitness),len(Fitness)))
+
 for i in range(len(Fitness)):
     for j in range(i,len(Fitness)):
         if i == j:
             classification_Table[i,j] = 0
             continue  
-        if Fitness[i]>Fitness[j]:
+        if Fitness[i][0]>Fitness[j][0]:
             classification_Table[i,j] = 1
             classification_Table[j,i] = -1
         else:
             classification_Table[i,j] = -1
             classification_Table[j,i] = 1
 
+selection = []
+
+
+for i in range(len(Fitness)):
+    for j in range(len(Fitness)):
+        if(j==i):
+            continue
+        res = []
+        res.append(classification_Table[i][j])
+        for k in range(1,len(Fitness[i])):
+            res.append(Fitness[i][k]-Fitness[j][k])
+        selection.append(res)
+
+selection = np.array(selection)
--- source/machine_learning.py
+++ source/machine_learning.py
@@ -7,52 +7,55 @@ from sklearn import preprocessing
 from sklearn.datasets import make_blobs
 from sklearn.inspection import DecisionBoundaryDisplay
 
+
+
+
+
+X=gs.selection[:,1:]
+Y = np.transpose(gs.selection[:,:1])
 # we create 40 separable points
-X = []
-y = []
-for v in range(len(gs.Macroparameters[1])):
-    for w in range(len(gs.Macroparameters[1])):
-        x=[]
-        if(w==v):
-            continue
-        if (gs.classification_Table[v][w]==1):
-            for i in range(8):
-                x.append(gs.Macroparameters[i][v]-gs.Macroparameters[i][w])
-            for i in range(8):
-                for j in range(i,8):
-                    x.append(gs.M_sqr[i][j][v]-gs.M_sqr[i][j][w])
-            y.append(gs.classification_Table[v][w])
-            X.append(x)
-        if (gs.classification_Table[v][w]==-1):
-            for i in range(8):
-                x.append
-            for i in range(8):
-                for j in range(i,8):
-                    x.append(gs.M_sqr[i][j][w]-gs.M_sqr[i][j][v])
-            y.append(gs.classification_Table[v][w])
-            X.append(x)
+# X = []
+# y = []
+# for v in range(len(gs.Macroparameters[1])):
+#     for w in range(len(gs.Macroparameters[1])):
+#         x=[]
+#         if(w==v):
+#             continue
+#         if (gs.classification_Table[v][w]!=0):
+#             for i in range(8):
+#                 x.append(gs.Macroparameters[i][v]-gs.Macroparameters[i][w])
+#             for i in range(8):
+#                 for j in range(i,8):
+#                     x.append(gs.M_sqr[i][j][v]-gs.M_sqr[i][j][w])
+#             y.append(gs.classification_Table[v][w])
+#             X.append(x)
                     
 
-print(X[1])
-X= fn.transpose(X)
-print(len(X[1]))
-print(X[1])
-res=[[]for i in range(len(X))]
-for i in range(len(X)):
-    max_param = max([abs(mp) for mp in X[i]])
-    for j in range(len(X[i])):
-        res[i].append(X[i][j]/max_param)
-X =res
-print(X[1])
-X = fn.transpose(res)
+
+                    
+# for i in range(len(X)):
+#     print(len(X[i]))
+
+# print(X[1])
+# X= fn.transpose(X)
+# print(len(X[1]))
+# print(X[1])
+# res=[[]for i in range(len(X))]
+# for i in range(len(X)):
+#     max_param = max([abs(mp) for mp in X[i]])
+#     for j in range(len(X[i])):
+#         res[i].append(X[i][j]/max_param)
+# X =res
+# print(X[1])
+# X = fn.transpose(res)
 
 
 
 
 
 # fit the model, don't regularize for illustration purposes
-clf = svm.SVC(kernel="linear", C=1000)
-clf.fit(X, y)
+# clf = svm.SVC(kernel="linear", C=1000)
+# clf.fit(X, y)
 
 # plt.scatter(X[:, 0], X[:, 5], c=y, s=3, cmap=plt.cm.Paired)
 
@@ -70,19 +73,19 @@ ax = plt.gca()
 # )
 # plot support vectors
 
-lambdas = clf.coef_[0]
-b = clf.intercept_[0]
-x_visual = np.linspace(-1,1)
-y_visual = -(lambdas[0] / lambdas[5]) * x_visual - b / lambdas[5]
-ax.plot(x_visual, y_visual)
+# lambdas = clf.coef_[0]
+# b = clf.intercept_[0]
+# x_visual = np.linspace(-1,1)
+# y_visual = -(lambdas[0] / lambdas[5]) * x_visual - b / lambdas[5]
+# ax.plot(x_visual, y_visual)
 # ax.scatter(
 #     clf.support_vectors_[:, 0],
 #     clf.support_vectors_[:, 5],
 #     s=100,
+
 #     linewidth=1,
 #     facecolors="none",
 #     edgecolors="k",
 # )
-X=np.array(X)
-ax.scatter(X[:, 0], X[:, 5], c=y, s=3, cmap=plt.cm.Paired)
+ax.scatter(X[:, 0], X[:, 5], c=Y, s=3, cmap=plt.cm.Paired)
 plt.show()
